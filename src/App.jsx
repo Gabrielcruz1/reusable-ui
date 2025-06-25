0c0259b9a558b3894e28d1a787e69fc01b16c134
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2025-06-24 21:43:39 -0700

    modal state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import './App.css'
+import Modal from "./Components/modal"
+import {useState} from "react"
 
 function App() {
 
+  const [open, setOpen] = useState(false)
+
+  const handleModal = () => {
+    if(open) {
+      setOpen((prev) => false)
+    } else {
+      setOpen((prev) => true)
+
+    }
+  }
+
   return (
     <>
+     <h1>Reusable UI</h1> <br/>
+     <button onClick={handleModal}>Open the Modal</button>
+     <Modal isOpen={open} />
     </>
   )
 }
